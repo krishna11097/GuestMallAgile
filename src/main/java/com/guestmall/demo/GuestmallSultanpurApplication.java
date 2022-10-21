c679c953e0ca85cdaf1484f9c501d528540355ee
Pratap Singh <k.g.pratap.singh@accenture.com>
2022-10-21 16:30:54 +0530

    first commit

---
+++ src/main/java/com/guestmall/demo/GuestmallSultanpurApplication.java
@@ -0,0 +1,13 @@
+package com.guestmall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestmallSultanpurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestmallSultanpurApplication.class, args);
+	}
+
+}
